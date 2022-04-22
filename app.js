7124595b8a22e6f2c4f36ab3cea67860dda8271f
omar2004712 <o.bahutair@gmail.com>
2022-04-22 17:51:03 +0400

    added animation for result

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const dict = [
-  'CLEAN',
-  'APPLE',
-  'WRITE',
-  'VAPOR',
-  'ACTOR'
-]
+document.querySelector('#words').onselectstart = new Function ("return false")
+document.querySelector('#words').style.cursor = 'default'
+let allWords = `ABUSE
+ADULT
+AGENT
+ANGER
+APPLE
+AWARD
+BASIS
+BEACH
+BIRTH`
+const dict = allWords.split('\n')
 
-const correctWord = dict[Math.floor(Math.random()*dict.length)]
+const correctWord = dict[Math.floor(Math.random()*dict.length)];
 
-function isWordCorrect(correctWord ,word){
-  return word===correctWord;
-}
 
 function isWordValid(dict, word){
   if(dict.indexOf(word) === -1){
@@ -32,15 +34,35 @@ let word = '';
 
 function clearWord(word){ //word element
   for(let char of word.children){
-    char.innerText = '';
+    char.querySelector('.letter').innerText = '';
+    char.classList.remove('resize')
   }
 }
+
+
+function flipLetters(wordEl) //wordEl
+{
+  const colors = getBackGroundColor(correctWord, word);
+  const borderColors = getBorderColor(correctWord, word);
+  for(let i = 0; i < wordEl.children.length; i++){
+    setTimeout(()=>{
+      let char = wordEl.children[i];
+      char.style.transform = 'rotateX(180deg)';
+      setTimeout(()=>{char.style.backgroundColor = colors[i];
+      char.style.borderColor = borderColors[i];
+      char.querySelector('.letter').style.transform = 'scale(1, -1)';
+      }, 150)
+    }, 
+      200*i)
+  }
+}
+
 
 function getBackGroundColor(correctWord, word){
   const colors = [];
   const green = 'rgb(84, 177, 84)';
   const gray = 'rgba(255, 255, 255, 10%)';
-  const yellow = 'rgb(175, 184, 58)';
+  const yellow = 'rgba(240, 225, 41, 80%)';
   for(let char of word){
     if(correctWord.indexOf(char) == word.indexOf(char)){
       colors.push(green);
@@ -59,7 +81,7 @@ function getBorderColor(correctWord, word){
   const colors = [];
   const green = 'rgb(84, 177, 84)';
   const gray = 'rgba(255, 255, 255, 0.1%)';
-  const yellow = 'rgb(175, 184, 58)';
+  const yellow = 'rgba(228, 225, 41, 80%)';
   for(let char of word){
     if(correctWord.indexOf(char) == word.indexOf(char)){
       colors.push(green);
@@ -78,7 +100,7 @@ function game(event){
   if(wordIdx !== 6){ 
     if(isLetterValid(event.key.toUpperCase())){
         if(charIdx <= 4){
-          words[wordIdx].querySelectorAll('.char')[charIdx].innerText = event.key.toUpperCase();
+          words[wordIdx].querySelectorAll('.char')[charIdx].querySelector('.letter').innerText = event.key.toUpperCase();
           words[wordIdx].querySelectorAll('.char')[charIdx].style.borderColor = 'white';
           words[wordIdx].children[charIdx].classList.add('resize');
           charIdx++;
@@ -86,34 +108,25 @@ function game(event){
         }
     } 
     else if(event.key.toUpperCase() === 'ENTER'){
-      if(isWordCorrect(correctWord, word)){
-        console.log("you win");
-        const result = document.createElement('div');
-        result.innerText = 'You Win';
-        result.classList.add('result');
-        const words = document.querySelector('#words');
-        words.append(result);
-        document.removeEventListener('keypress', game)
-      }
+      const result = document.createElement('div');
+      result.classList.add('result');
       if(isWordValid(dict, word)){
-        const colors = getBackGroundColor(correctWord, word);
-        const borderColors = getBorderColor(correctWord, word);
-        for(let i=0; i < 5; i++){
-          let color = colors[i];
-          let borderColor = borderColors[i];
-          words[wordIdx].children[i].style.backgroundColor = color;
-          words[wordIdx].children[i].style.borderColor = borderColor;
+        flipLetters(words[wordIdx])
+        {const words = document.querySelector('#words');
+        if(correctWord == word){
+          result.innerText = 'You Win';
+          document.removeEventListener('keypress', game)
+          setTimeout(words.append(result), 1500);
         }
-        wordIdx++;
-        if(wordIdx == 6){
-          const result = document.createElement('div');
+        if(wordIdx == 5){
           result.innerText = correctWord;
-          result.classList.add('result');
-          const words = document.querySelector('#words');
-          words.append(result);
+          setTimeout(()=>{words.append(result)}, 1500);
+          document.removeEventListener('keypress', game)
+        }
         }
         word=''
         charIdx = 0;
+        wordIdx++;
       }
       else{
         if(word.length === 5){
@@ -139,7 +152,7 @@ document.addEventListener('keyup', (event)=>{
   if (event.key == 'Backspace'){
     if(charIdx != 0){
       charIdx--;
-      words[wordIdx].children[charIdx].innerText = '';
+      words[wordIdx].children[charIdx].querySelector('.letter').innerText = '';
       words[wordIdx].children[charIdx].classList.remove('resize');
       words[wordIdx].children[charIdx].style.borderColor = 'rgba(255, 255, 255, 10%)';
       const arr = word.split('');
